0ea6d092aeaaa407a7b67de922fda68f6492e073
Nicolas Mamet <nicolas.mamet.ext@eulerhermes.com>
2025-03-23 19:33:00 +0100

    fix: improve init script

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 npm install || exit 1
-npm install --workspace core || exit 2
-npm install --workspace database || exit 3
-npm install --workspace back || exit 4
 cd database
 node createDb.js
