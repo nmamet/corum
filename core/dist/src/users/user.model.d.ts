cffd5f1f6120e5553909018fe4729eabc77f32a9
Nicolas Mamet <nicolas.mamet.ext@eulerhermes.com>
2025-03-23 18:17:47 +0100

    config: scripts

--- core/dist/src/users/user.model.d.ts
+++ core/dist/src/users/user.model.d.ts
@@ -1,6 +1,9 @@
-export type User = {
-  email: string;
-  firstName: string;
-  lastName: string;
-  birthDate: number;
+export type UserInfo = {
+    email: string;
+    firstName: string;
+    lastName: string;
+    birthDate: number;
+};
+export type User = UserInfo & {
+    password: string;
 };
--- core/src/users/user.model.ts
+++ core/src/users/user.model.ts
@@ -1,5 +1,3 @@
-import z from "zod";
-
 export type UserInfo = {
   email: string;
   firstName: string;
